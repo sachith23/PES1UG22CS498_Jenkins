3387e55c51cb5bc6bc41ef108340a6ff1c52866e
Sachithsagar <132151018+sachith23@users.noreply.github.com>
2025-03-18 16:21:47 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int num1, num2, sum;
+
+    // Taking input from the user
+    std::cout << "Enter first number: ";
+    std::cin >> num1;
+    std::cout << "Enter second number: ";
+    std::cin >> num2;
+
+    // Calculating sum
+    sum = num1 + nu
+
+    // Displaying result
+    std::cout << "Sum: " << sum << std::endl;
+
+    return 0;
+}
